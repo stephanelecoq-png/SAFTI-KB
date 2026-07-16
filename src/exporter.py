f24dc01608d0de03bf7e46548ad8046e54816aaa
Stéphane LECOQ <stephane.lecoq@safti.fr>
2026-07-16 16:00:51 +0200

    feat: générer un premier fichier Markdown

--- src/exporter.py
+++ src/exporter.py
@@ -1,8 +1,24 @@
+from pathlib import Path
+
+from src.markdown import article_to_markdown
 from src.parser import CSVParser
+from src.utils import write_text
 
 
 def exporter(csv_file: str):
     parser = CSVParser(csv_file)
     articles = parser.parse()
 
+    if not articles:
+        print("Aucun article trouvé.")
+        return
+
+    contenu = article_to_markdown(articles[0])
+
+    write_text(
+        Path("test.md"),
+        contenu
+    )
+
+    print("Fichier test.md généré.")
     print(f"{len(articles)} article(s) chargé(s).")
